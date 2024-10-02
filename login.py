fed33e6487a4031c11c1278191d8905e1990e7e9
ingeniarperuempresa <peruingeniar@gmail.com>
2024-10-02 22:18:50 +0200

    Update login.py

--- login.py
+++ login.py
@@ -52,4 +52,4 @@ if st.session_state.get("logged_in"):
     st.write(f"Hola {st.session_state.nombre}, tu sueño es: {st.session_state.sueños}.")
 else:
     st.warning("👈 Despliega el panel lateral para iniciar sesión.")
-    st.image("logo.png", width=100)  # Asegúrate de que la imagen esté en la ruta correcta
+    st.image("logo2.png", width=100)  # Asegúrate de que la imagen esté en la ruta correcta
